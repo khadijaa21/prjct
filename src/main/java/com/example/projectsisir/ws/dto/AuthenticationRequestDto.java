e5100d2a928fdc41a1fd0a3320f501b8b0ee90e5
abdelhayel <74475315+abdelhayel@users.noreply.github.com>
2023-04-15 02:06:31 +0000

    Add files via upload

---
+++ src/main/java/com/example/projectsisir/ws/dto/AuthenticationRequestDto.java
@@ -0,0 +1,16 @@
+package taxes.ws.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequestDto {
+private String email;
+    private String username;
+    String password;
+}
